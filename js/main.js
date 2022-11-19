d6a51b9c287996e80f65d8e31919761459d9d60a
fabi-hiago <fabiohr1600@gmail.com>
2022-11-19 19:51:13 -0300

    .

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//carousel
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
